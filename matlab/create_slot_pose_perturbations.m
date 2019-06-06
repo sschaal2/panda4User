c151a11bf98a3e662c63d2f30196890bd571060f
Stefan Schaal <sschaal@usc.edu>
2019-06-06 14:07:35 -0700

    small fixes

---
+++ matlab/create_slot_pose_perturbations.m
@@ -0,0 +1,25 @@
+function X = create_slot_pose_perturbations(fname)
+% this creates an array of relative slot pose perurbations and saves this
+% in a file for the sm_task
+
+max_x_mm = 2/2;
+max_y_mm = 2/2;
+max_z_mm = 4/2;
+max_orient_degree = 5/2.5;
+
+X = [];
+
+for x=-max_x_mm:max_x_mm
+    for y=-max_y_mm:max_y_mm
+        for z=-max_z_mm:max_z_mm
+            pose_x = [x y z]/1000;
+            angle = randi([-max_orient_degree max_orient_degree],1)/180*pi;
+            vec = rand(3,1)*2-1;
+            vec = vec/norm(vec);
+            pose_q = [cos(angle/2) vec'*sin(angle/2)];
+            X = [X; [pose_x pose_q]];
+        end
+    end
+end
+
+save(fname,'X','-ascii');
