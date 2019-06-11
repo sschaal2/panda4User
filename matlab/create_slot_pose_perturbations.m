c7e223b0706b563896e06347bbfc1331f56d0e36
sschaal <sschaal@usc.edu>
2019-06-11 15:25:21 -0700

    read for vision data collection

--- matlab/create_slot_pose_perturbations.m
+++ matlab/create_slot_pose_perturbations.m
@@ -7,11 +7,18 @@ max_y_mm = 2/2;
 max_z_mm = 4/2;
 max_orient_degree = 5/2.5;
 
+max_x_mm = 10;
+max_y_mm = 10;
+max_z_mm = 5;
+max_orient_degree = 5;
+
 X = [];
 
-for x=-max_x_mm:max_x_mm
-    for y=-max_y_mm:max_y_mm
-        for z=-max_z_mm:max_z_mm
+s = 2;
+
+for x=-max_x_mm:s:max_x_mm
+    for y=-max_y_mm:s:max_y_mm
+        for z=-max_z_mm:s:max_z_mm
             pose_x = [x y z]/1000;
             angle = randi([-max_orient_degree max_orient_degree],1)/180*pi;
             vec = rand(3,1)*2-1;
