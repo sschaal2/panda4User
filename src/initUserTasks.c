0e9fc64aa7ca59c0ed8d9e1d0ab407e9df6111ed
Stefan Schaal <stefan.k.schaal@gmail.com>
2022-06-28 15:10:36 -0700

    replace endeffector with stick

--- src/initUserTasks.c
+++ src/initUserTasks.c
@@ -27,8 +27,6 @@ static int user_tasks_initialized = FALSE;
 int no_user_interaction_flag = FALSE;
 
 // local functions
-static void runInsertions(void);
-
 
 /*****************************************************************************
 ******************************************************************************
@@ -51,85 +49,12 @@ void
 initUserTasks(void)
 
 {
-  extern void add_sm_task();
   extern void add_gravcomp_task();
-  extern void add_test_task();
-  extern void add_impedance_test_task();
-  extern void add_cilantro_track_task();
-
 
   add_gravcomp_task();
-  add_sm_task();
-  add_test_task();
-  add_impedance_test_task();
-  add_cilantro_track_task();
-
-  addToMan("run_insertions","runs several insertion state-machines",runInsertions);
 
   //sprintf(initial_user_command,"go0");
 
 }
 
  
-/*****************************************************************************
-******************************************************************************
-Function Name   : runTrials
-Date            : Sept 2006
-Remarks:
-
-       runs several insertion state machines in concatention
-
-
-******************************************************************************
-Paramters:  (i/o = input/output)
-
-   none
-
-*****************************************************************************/
-static void
-runInsertions(void) {
-
-  int i;
-  int n_iterations = 1;
-  extern int sm_run_table;
-  extern char sm_file_name[];
-
-  get_int("How many iterations?",n_iterations,&n_iterations);
-
-  for (i=1; i<=n_iterations+1; ++i) {
-
-    while (strcmp(current_task_name,NO_TASK) != 0)
-      sleep(1);
-
-    if (i==n_iterations+1)
-      break;
-
-    no_user_interaction_flag = TRUE;
-    sm_run_table = TRUE;
-    if (i%6 == 1)
-      strcpy(sm_file_name,"powerplug_JT_1.sm");
-    else if (i%6 == 2)
-      strcpy(sm_file_name,"hdmi_JT_1.sm");
-    else if (i%6 == 3)
-      strcpy(sm_file_name,"usb_JT_1.sm");
-    else if (i%6 == 4)
-      strcpy(sm_file_name,"hdmi_JT_2.sm");
-    else if (i%6 == 5)
-      strcpy(sm_file_name,"usb_JT_2.sm");
-    else if (i%6 == 0)
-      strcpy(sm_file_name,"powerplug_JT_2.sm");
-
-
-    setTaskByName("State Machine Task");
-
-    while (strcmp(current_task_name,NO_TASK) == 0)
-      sleep(1);
-    
-    printf("========= Restart %d ==========\n",i);
-
-  }
-
-  printf("\n\n All done\n");
-
-  no_user_interaction_flag = FALSE;
-}
